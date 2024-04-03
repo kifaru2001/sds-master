123a7cdb427d5417336f97cb95417aa85ccac233
Urchin <165049108+asa21212@users.noreply.github.com>
2024-04-03 09:53:10 +0000

    121212

--- components/intro1.js
+++ components/intro1.js
@@ -110,7 +110,6 @@ const Intro = (props) => {
             flex-direction: column;
             background-size: cover;
             justify-content: flex-start;
-            z-index: -1;
             background-image: linear-gradient(
                 180deg,
                 rgba(0, 0, 0, 0.68) 0%,
