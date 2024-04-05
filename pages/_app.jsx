a2160b3b0186d5764965861753cc64d616189ccb
Urchin <165049108+asa21212@users.noreply.github.com>
2024-04-05 03:27:18 +0000

    1212

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -85,3 +85,4 @@ export default function MyApp({ Component: Component, pageProps: pageProps }) {
   
   )
 }
+
--- pages/step-two.js
+++ pages/step-two.js
@@ -19,7 +19,9 @@ import { contractAddress } from "../const/yourDetails";
 
 import checkBalance from "../util/checkBalance";
 import CoreBlockchain from "@thirdweb-dev/chains"
+
 const StepTwo = (props) => {
+
   const { contract } = useContract(contractAddress);
   const { data: contractMetadata, isLoading: contractLoading } =
     useContractMetadata(contract);
@@ -32,6 +34,7 @@ const StepTwo = (props) => {
       router.push("/verify");
     }
   }, [nfts, router, address]);
+
   return (
     <>
       <div className="step-two-container">
@@ -77,3 +80,38 @@ const StepTwo = (props) => {
 }
 
 export default StepTwo
+
+
+export async function getServerSideProps(context) {
+  const user = await getUser(context.req);
+
+  if (!user) {
+    return {
+      props: {},
+    };
+  }
+
+  const secretKey = process.env.TW_SECRET_KEY;
+
+  if (!secretKey) {
+    console.log("Missing env var: TW_SECRET_KEY");
+    throw new Error("Missing env var: TW_SECRET_KEY");
+  }
+
+  // Ensure we are able to generate an auth token using our private key instantiated SDK
+  const PRIVATE_KEY = process.env.THIRDWEB_AUTH_PRIVATE_KEY;
+  if (!PRIVATE_KEY) {
+    throw new Error("You need to add an PRIVATE_KEY environment variable.");
+  }
+
+  // Instantiate our SDK
+  const sdk = ThirdwebSDK.fromPrivateKey(
+    process.env.THIRDWEB_AUTH_PRIVATE_KEY,
+    CoreBlockchain,
+    { secretKey }
+  );
+
+  return {
+    props: {},
+  };
+}
